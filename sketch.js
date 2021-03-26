fca5dfdc0e5186c3b74ff049ba1e41ee2b504264
Anushka202 <74297186+Anushka202@users.noreply.github.com>
2021-03-26 13:16:39 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var database;
+var playerCount, form, player, game;
+var gameState = 0;
+var canvas, bgImg
+
+
+function setup(){
+ camvas = createCanvas(400, 400);
+
+
+  database = firebase.database();
+  console.log(database);
+  
+}
+
+function draw(){
+  
+  
+}
+
